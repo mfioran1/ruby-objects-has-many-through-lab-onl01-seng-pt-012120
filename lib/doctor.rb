6605f71e225ec35ceb367f670857aec6968be05a
Maurice Fiorani <mauricef97@gmail.com>
2020-02-21 18:50:32 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end  
   
   def new_appointment(patient, date)
-    appointment = Appointment.new(patient, self, date)
+    appointment = Appointment.new(date, patient, self)
   end
     
   def patients
